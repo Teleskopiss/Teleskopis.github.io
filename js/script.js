ed7a194eed5b76d3329f2df716e0ac19fee058d9
Teleskopis <118316745+Teleskopiss@users.noreply.github.com>
2023-08-15 17:08:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-slots.forEach(slot => {
-    slot.addEventListener('click', () => {
-        const itemName = prompt('Enter item name:');
-        const itemQuantity = parseInt(prompt('Enter item quantity:'));
+// script.js
+const searchBar = document.getElementById('searchBar');
+const suggestionsContainer = document.getElementById('suggestions');
 
-        // Update slot content with user input
-        const lowercaseItemName = itemName.toLowerCase().replace(/\s/g, '_'); // Convert to lowercase and replace spaces with underscores
-        const itemImageSrc = `images/${lowercaseItemName}.png`;
+const itemNames = ["diamond sword", "iron pickaxe", "golden apple", /* Add more item names */];
 
-        // Update slot content
-        slot.querySelector('img').src = itemImageSrc;
-        slot.querySelector('input[type="text"]').value = itemName;
-        slot.querySelector('input[type="number"]').value = itemQuantity;
-    });
+searchBar.addEventListener('input', () => {
+    const searchText = searchBar.value.toLowerCase();
+    const matchedItems = itemNames.filter(itemName => itemName.toLowerCase().includes(searchText));
+
+    displaySuggestions(matchedItems);
 });
+
+function displaySuggestions(suggestions) {
+    suggestionsContainer.innerHTML = '';
+
+    suggestions.forEach(suggestion => {
+        const suggestionElement = document.createElement('div');
+        suggestionElement.textContent = suggestion;
+        suggestionElement.classList.add('suggestion');
+
+        suggestionElement.addEventListener('click', () => {
+            searchBar.value = suggestion;
+            suggestionsContainer.innerHTML = '';
+        });
+
+        suggestionsContainer.appendChild(suggestionElement);
+    });
+}
