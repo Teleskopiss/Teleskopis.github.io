d09970729b9b413d23180b87dde958ff840cd647
Teleskopis <118316745+Teleskopiss@users.noreply.github.com>
2023-08-15 12:33:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const slots = document.querySelectorAll('.inventory-slot');
 
+slots.forEach(slot => {
+    slot.addEventListener('click', () => {
+        const itemName = prompt('Enter item name:');
+        const itemQuantity = parseInt(prompt('Enter item quantity:'));
+
+        // Update slot content with user input
+        slot.querySelector('img').src = `images/${itemName}.png`;
+        slot.querySelector('input[type="text"]').value = itemName;
+        slot.querySelector('input[type="number"]').value = itemQuantity;
+    });
+});
