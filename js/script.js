54405f212cfda5e5a045a130f463346c6001064e
Teleskopis <118316745+Teleskopiss@users.noreply.github.com>
2023-08-15 17:17:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,28 +2,21 @@
 const searchBar = document.getElementById('searchBar');
 const suggestionsContainer = document.getElementById('suggestions');
 
-const itemNames = ["diamond sword", "iron pickaxe", "golden apple", /* Add more item names */];
+let itemData = []; // To store fetched item data
+
+// Fetch item data from the JSON file
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        itemData = data;
+    })
+    .catch(error => console.error('Error fetching item data:', error));
 
 searchBar.addEventListener('input', () => {
     const searchText = searchBar.value.toLowerCase();
-    const matchedItems = itemNames.filter(itemName => itemName.toLowerCase().includes(searchText));
+    const matchedItems = itemData.filter(item => item.name.toLowerCase().includes(searchText));
 
     displaySuggestions(matchedItems);
 });
 
-function displaySuggestions(suggestions) {
-    suggestionsContainer.innerHTML = '';
-
-    suggestions.forEach(suggestion => {
-        const suggestionElement = document.createElement('div');
-        suggestionElement.textContent = suggestion;
-        suggestionElement.classList.add('suggestion');
-
-        suggestionElement.addEventListener('click', () => {
-            searchBar.value = suggestion;
-            suggestionsContainer.innerHTML = '';
-        });
-
-        suggestionsContainer.appendChild(suggestionElement);
-    });
-}
+// Rest of the code remains the same
