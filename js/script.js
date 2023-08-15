04b5bf2dc1e4efc6d27e531b72c83f2e33f69d3a
Teleskopis <118316745+Teleskopiss@users.noreply.github.com>
2023-08-15 17:06:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-const slots = document.querySelectorAll('.inventory-slot');
-
 slots.forEach(slot => {
     slot.addEventListener('click', () => {
         const itemName = prompt('Enter item name:');
         const itemQuantity = parseInt(prompt('Enter item quantity:'));
 
         // Update slot content with user input
-        slot.querySelector('img').src = `images/${itemName}.png`;
+        const lowercaseItemName = itemName.toLowerCase().replace(/\s/g, '_'); // Convert to lowercase and replace spaces with underscores
+        const itemImageSrc = `images/${lowercaseItemName}.png`;
+
+        // Update slot content
+        slot.querySelector('img').src = itemImageSrc;
         slot.querySelector('input[type="text"]').value = itemName;
         slot.querySelector('input[type="number"]').value = itemQuantity;
     });
